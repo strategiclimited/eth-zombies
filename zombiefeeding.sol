b7aa3852edc01b55660ba7dabbd996aaedcbb950
strategiclimited <strategiclimited@users.noreply.github.com>
2017-12-28 14:34:15 -0500

    new zombie after consume dna 2.9

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -7,7 +7,9 @@ contract ZombieFeeding is ZombieFactory {
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
-    // start here
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    _createZombie("NoName", newDna);
   }
 
 }
